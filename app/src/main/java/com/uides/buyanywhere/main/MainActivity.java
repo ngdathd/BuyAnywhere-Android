f7563c7622629626082a5caaad0803ec4be464c9
TranThanhTung <thanhtung100397@gmail.com>
2017-11-08 09:46:30 +0700

    All

--- app/src/main/java/com/uides/buyanywhere/main/MainActivity.java
+++ app/src/main/java/com/uides/buyanywhere/main/MainActivity.java
@@ -8,14 +8,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
